19b847f5100bb0cd67de8e706ef69a6fdc2a0c64
Toni Guli <pllumiguli@icloud.com>
2021-10-15 15:16:07 +0200

    removed phitespacef from json, error condition

--- src/json_parsing.rs
+++ src/json_parsing.rs
@@ -1,23 +1,36 @@
 use std::fs::File;
 use std::io::Read;
+use std::io::{Error, ErrorKind};
+use crate::converter::Convert;
 
 pub fn run() {
-	let raw_file_data = match read_file(String::from("./data/data.json")) {
+	let mut raw_file_data = match read_file(String::from("./data/data.json")) {
 		Ok(v)	=> v,
 		Err(e)		=> panic!("error while reading the file: {}", e)
 	};
 
-	dbg!(raw_file_data);
+	parse_data(&mut raw_file_data);
 }
 
 fn read_file(filename: String) -> Result<String, Box<dyn std::error::Error>> {
 	let mut file_handler = File::open(filename)?;
 	let mut out = String::new();
 
-	file_handler.read_to_string(&mut out);
+	file_handler.read_to_string(&mut out)?;
 	
 	Ok(out)
 }
 
-fn parse_data(json: String) {
+fn parse_data(json: &mut String) -> Result<Vec<Convert>, Box<dyn std::error::Error>>{
+	// remove whitespaces from String
+	json.retain(|c| !c.is_whitespace());
+	println!("{}", json);
+
+	let converts: Vec<Convert> = vec![];
+
+	if json.chars().nth(0).unwrap() != '{' {
+		return Err(Box::new(Error::new(ErrorKind::InvalidData, "invalid JSON data!")));
+	}
+
+	Ok(converts)
 }
