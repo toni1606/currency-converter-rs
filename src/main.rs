54d985754e3d0b2ef40692a251c63655d3ed78eb
Toni Guli <pllumiguli@icloud.com>
2021-10-16 19:59:02 +0200

    formatted output

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use currency_converter::json_parsing;
 
 fn main() {
 	match json_parsing::run("./data/data.json") {
-		Ok(v)	=> println!("value: {}", v),
+		Ok(v)	=> println!("value: {:.3}", v),
 		Err(e)	=> panic!("{}", e)
 	}
 }
